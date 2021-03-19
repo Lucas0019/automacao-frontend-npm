f8362faa4332887f71fd28b039c068e56974b424
Lucas <lxavier.00@outlook.com>
2021-03-18 21:31:38 -0300

    Criado e Add mini task sass com gulp - autoprefizer

--- project-gulp/gulpfile.js
+++ project-gulp/gulpfile.js
@@ -1,11 +1,18 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+// const autoprefixer = require('gulp-autoprefixer');
 
 // Tarefas
 function complilaStyles() {
   return gulp
     .src('styles/scss/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    // .pipe(autoprefixer({
+    //   browsers: ['last 2 versions'],
+    //   cascade: false
+    // }))
     .pipe(gulp.dest('styles/'))
 }
 
